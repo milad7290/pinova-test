14bc26584a6994813ff379f75a0b5f25936921f0
milad <sakhaei90.itsu@gmail.com>
2019-04-08 23:59:45 +0430

    table to div

--- client/invoice/report/InvoiceTable.js
+++ client/invoice/report/InvoiceTable.js
@@ -24,6 +24,62 @@ const styles = theme => ({
   table: {
     minWidth: 700,
   },
+  rTable :{
+  	display: 'block',
+  	width: '100%',
+},
+rTableHeading:{
+  clear: 'both',
+}
+, rTableBody:{
+  clear: 'both',
+}
+, rTableFoot:{
+  clear: 'both',
+}, 
+rTableRow:{
+    clear: 'both',
+  borderBottom: '1px solid rgba(224, 224, 224, 1)',
+  backgroundColor: '#fafafa'
+},
+rTableHead:{
+  backgroundColor:'#DDD',
+}, rTableFoot:{
+  backgroundColor:'#DDD',
+},
+rTableCell1:{
+  display: 'inline-block',
+  height: 40,
+  textAlign: 'center',
+  padding: '10px',
+  width: '25%',
+}
+, rTableHead1 :{
+  backgroundColor:'black',
+  color:'white',
+  	display: 'inline-block',
+  	height: 40,
+  	textAlign: 'center',
+    padding: '9px 0px 0px 0px',
+  	width: '25%',
+}
+,
+rTableCell2:{
+  display: 'inline-block',
+  height: 40,
+  textAlign: 'center',
+  padding: '10px',
+  width: '20%',
+}
+, rTableHead2 :{
+  backgroundColor:'black',
+  color:'white',
+  	display: 'inline-block',
+  	height: 40,
+  	textAlign: 'center',
+    padding: '9px 0px 0px 0px',
+  	width: '20%',
+}
 });
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -57,47 +113,44 @@ class InvoiceTable extends React.Component {
     <Paper className={classes.root}>
       
        {(this.props.filterType!=='factorItems')?(
-         <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <CustomTableCell>نام خریدار</CustomTableCell>
-            <CustomTableCell align="right">مبلغ</CustomTableCell>
-            <CustomTableCell align="right">شماره فاکتور</CustomTableCell>
-            <CustomTableCell align="right">زمان تحویل</CustomTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+         <div className={classes.rTable}>
+            <div className={classes.rTableRow}>
+                <div className={classes.rTableHead1}><strong>نام خریدار</strong></div>
+                <div className={classes.rTableHead1}><strong>مبلغ</strong></div>
+                <div className={classes.rTableHead1}><strong>شماره فاکتور</strong></div>
+                <div className={classes.rTableHead1}><strong>زمان تحویل</strong></div>
+            </div>
           {this.props.invoices.map(row => (
-            <TableRow key={row._id}>
-              <CustomTableCell component="th" scope="row">
+            <div className={classes.rTableRow} key={row._id}>
+              <div className={classes.rTableCell1}>
                 {row.invoiceCustomer}
-              </CustomTableCell>
-              <CustomTableCell align="right">{numberWithoutCommas(row.totalPrice)}</CustomTableCell>
-              <CustomTableCell align="right">{persianNumber(row.invoiceNumber)}</CustomTableCell>
-              <CustomTableCell align="right">{moment(row.deliveryDate).format('dddd jDo jMMMM  hh a') }</CustomTableCell>
-            </TableRow>
+              </div>
+              <div className={classes.rTableCell1}>{numberWithoutCommas(row.totalPrice)}</div>
+              <div className={classes.rTableCell1}>{persianNumber(row.invoiceNumber)}</div>
+              <div className={classes.rTableCell1}>{moment(row.deliveryDate).format('dddd jDo jMMMM ساعت hh:mm') }</div>
+            </div>
           ))}
-        </TableBody></Table>)
-        :(<Table className={classes.table}>
-        <TableHead>
-        <TableRow>
-          <CustomTableCell>نام خریدار</CustomTableCell>
-          <CustomTableCell align="center">نام محصول</CustomTableCell>
-          <CustomTableCell align="center">شماره فاکتور</CustomTableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
+          </div>)
+        :(<div className={classes.rTable}>
+            <div className={classes.rTableRow}>
+                <div className={classes.rTableHead2}><strong>نام خریدار</strong></div>
+                <div className={classes.rTableHead2}><strong>نام محصول</strong></div>
+                <div className={classes.rTableHead2}><strong>قیمت محصول</strong></div>
+                <div className={classes.rTableHead2}><strong>تعداد</strong></div>
+                <div className={classes.rTableHead2}><strong>شماره فاکتور</strong></div>
+            </div>
         {this.props.invoices.map(row => 
-          <TableRow key={row._id}>
-            <CustomTableCell component="th" scope="row">
+           <div className={classes.rTableRow} key={row._id}>
+            <div className={classes.rTableCell2}>
               {row.invoiceCustomer}
-            </CustomTableCell>
-            <CustomTableCell align="center">{row.productName}</CustomTableCell>
-            <CustomTableCell align="center">{persianNumber(row.invoiceNumber)}</CustomTableCell>
-          </TableRow>
+            </div>
+            <div className={classes.rTableCell2}>{row.productName}</div>
+            <div className={classes.rTableCell2}>{numberWithoutCommas(row.price)}</div>
+            <div className={classes.rTableCell2}>{persianNumber(row.count)}</div>
+            <div className={classes.rTableCell2}>{persianNumber(row.invoiceNumber)}</div>
+          </div>
         )}
-      </TableBody>
-      </Table>
+      </div>
        )}   
     </Paper>
   );
